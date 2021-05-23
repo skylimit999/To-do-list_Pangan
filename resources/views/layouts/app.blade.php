461ac00b0806d92a380233e625f81d31d13264be
Skylimit999 <jonathanpangan@gmail.com>
2021-05-23 23:39:51 +0800

    project todo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css')}}" rel="stylesheet">
+
+  <title>Todo App</title>
+<style type="text/css">
+	body{
+		background: url("{{ asset('image/todo-list-bg.jpg') }}");
+		background-size: cover;
+	}
+</style>
+
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
